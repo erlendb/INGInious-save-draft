ddfcadce627ec5650247c7aa4694157c696d8ba4
Erlend Blomseth <erlend@elendig.no>
2019-09-13 12:30:54 +0200

    fjernet spor av Mobius og kommentarer fra injector

--- src/injector.js
+++ src/injector.js
@@ -1,6 +1,4 @@
-// Möbius
 href = location.href;
-//hrefEnding = href.substr(href.lastIndexOf('/'));
 hrefTmp = href.substr(0,href.lastIndexOf('/'));
 hrefCourse = hrefTmp.substr(hrefTmp.lastIndexOf('/')).slice(1);
 if (hrefCourse == 'TDT4120') {
