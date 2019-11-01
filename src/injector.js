65eb50695825b36c1ad6e83eb420de1d5dd4f2f7
Erlend Blomseth <erlend@elendig.no>
2019-11-01 22:05:19 +0100

    Fix manifest matches. Injector now only injects content script when on an INGInious test

--- src/injector.js
+++ src/injector.js
@@ -1,5 +1,27 @@
 // Set global variables
-href = location.href;
+origin = window.location.origin;
+path = window.location.pathname;
 
-// Inject the content script
-inject_inginious();
+course = '';
+test = '';
+
+match = window.location.pathname.match(/^.*\/course\/([^\/]*)\/([^/?]*)/);
+if (match != null && match.length == 3) {
+	course = match[1];
+	test = match[2]
+}
+
+// Inject the content script if on a test page
+if (isINGInious() && isTest()) {
+	inject_inginious();
+}
+
+// Returns whether current page is an INGInious page or not
+function isINGInious() {
+	return ($('meta[name="description"]').attr('content') == 'INGInious');
+}
+
+// Returns whether current page is a test or not
+function isTest() {
+	return (test.length > 0);
+}
